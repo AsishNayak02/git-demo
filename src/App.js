e978213d23244349b6e4ac9b9f3480a99f26a250
AsishNayak02 <asish.nayak@eximietas.design>
2024-01-08 13:11:49 +0530

    feat(ui): add list unordered list added

--- src/App.js
+++ src/App.js
@@ -3,33 +3,40 @@ import useCustom from "./useCustom";
 import Stylesheet from './component/stylesheet';
 import Inline from './component/Inline';
 import './appStyles.css'
+import List from './List'
 import styles from './appStyles.module.css'
 import StyledBtn from './component/styled-comp';
 function App() {
-  const count = useCustom();
-  const chng=()=>{
-    console.log("clicked on button")
-  }
+  // const count = useCustom();
+  // const chng=()=>{
+  //   console.log("clicked on button")
+  // }
   return (
 
     <div>
-      <StyledBtn>Styled component</StyledBtn>
+      {/* <StyledBtn>Styled component</StyledBtn>
       <StyledBtn variant='outline'>Styled component</StyledBtn>
 
       <h1 className='error'>Error</h1>  {/*from css with global scope*/}
-      <h1 className={styles.success}>Success</h1> {/*from module with local scope*/}
-      <Stylesheet />
+      {/* <h1 className={styles.success}>Success</h1> {/*from module with local scope*/}
+      {/* <Stylesheet />
       <Inline />
       <div >custom hook :: {count}</div>
       <button onClick = {chng}>Click me</button> 
-      <h2>This is a new branch</h2>
-
-      <div>
-        <ol>
-          <li>1.List1</li>
-          <li>2.List2</li>
-          <li>3.List3</li>
-        </ol>
+      <h2>This is a new branch</h2>   */}
+      <div className='list-cont'>
+      <div  className='box'>
+       <List />
+      </div>
+      <div className='box'>
+       <List />
+      </div>
+      <div className='box'>
+       <List />
+      </div>
+      <div className='box'>
+       <List />
+      </div>
       </div>
     </div>
   );
